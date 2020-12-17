fc871a5bd8e59bacadc1d5300edf2e01ac871fa5
NGDK78 <karol.koson@motorolasolutions.com>
2020-12-17 21:45:27 +0100

    zestaw 2

--- Statystyka/Zestaw2/B.py
+++ Statystyka/Zestaw2/B.py
@@ -1,8 +1,7 @@
 import numpy as np
-import random
 
 
-def markov_chain(state, stepsMatrix, iterations):
+def markov_chain(state, steps_matrix, iterations):
     steps = [0, 1, 2]
 
     counter = {
@@ -13,23 +12,24 @@ def markov_chain(state, stepsMatrix, iterations):
 
     for i in range(iterations):
         if state == 0:
-            state = np.random.choice(steps, replace=True, p=stepsMatrix[0])
+            state = np.random.choice(steps, replace=True, p=steps_matrix[0])
             counter[str(state)] += 1
         elif state == 1:
-            state = np.random.choice(steps, replace=True, p=stepsMatrix[1])
+            state = np.random.choice(steps, replace=True, p=steps_matrix[1])
             counter[str(state)] += 1
         elif state == 2:
-            state = np.random.choice(steps, replace=True, p=stepsMatrix[2])
+            state = np.random.choice(steps, replace=True, p=steps_matrix[2])
             counter[str(state)] += 1
 
     return counter
 
 
-a = np.array([[0.64, 0.32, 0.04], [0.4, 0.5, 0.1], [0.25, 0.5, 0.25]])
-N = 10000
+# P = np.array([[0.64, 0.32, 0.04], [0.4, 0.5, 0.1], [0.25, 0.5, 0.25]])
+P = np.array([[0.2, 0.7, 0.1], [0.9, 0.0, 0.1], [0.2, 0.8, 0.0]])
+N = 100000
 
 step = 0
-counter = markov_chain(step, a, N)
+counter = markov_chain(step, P, N)
 
 print("Lancuch Markowa dla stanu poczatkowego: " + str(step))
 print(counter["0"] / N)
@@ -37,7 +37,7 @@ print(counter["1"] / N)
 print(counter["2"] / N)
 
 step = 1
-counter = markov_chain(step, a, N)
+counter = markov_chain(step, P, N)
 
 print("Lancuch Markowa dla stanu poczatkowego: " + str(step))
 print(counter["0"] / N)
@@ -45,7 +45,7 @@ print(counter["1"] / N)
 print(counter["2"] / N)
 
 step = 2
-counter = markov_chain(step, a, N)
+counter = markov_chain(step, P, N)
 
 print("Lancuch Markowa dla stanu poczatkowego: " + str(step))
 print(counter["0"] / N)
